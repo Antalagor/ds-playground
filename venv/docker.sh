690f49e6e2f582ed84612d00af84bfe21b7e1f08
p.wolfram <p.wolfram@emetriq.com>
2019-12-03 13:42:34 +0100

    nicer docker

--- venv/docker.sh
+++ venv/docker.sh
@@ -3,6 +3,10 @@ ACTION=${1:-""}
 if [ "$ACTION" = "build" ]
   then
     docker build -t rl-env .
-  else
-    docker run --rm  -e MOUNT=/root/mount -v ${PWD}:/root/mount -t -p 8888:8888 rl-env
+fi
+
+if [ "$ACTION" = "restart" ]
+  then
+    docker kill "$(docker ps | grep "rl-env" | awk '{ print $1 }')"
+    docker run --rm -d -e PYTHONPATH=/root/mount -v ${PWD}:/root/mount -t -p 8889:8888 bidder-rl-env
 fi
