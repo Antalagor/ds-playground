e1b704f3dc085dd2b0b8fc6b854c368a44e866ed
p.wolfram <p.wolfram@emetriq.com>
2020-01-06 17:40:03 +0100

    - better docker.sh

--- venv/docker.sh
+++ venv/docker.sh
@@ -1,12 +1,18 @@
-ACTION=${1:-""}
+ACTION=${1:-"restart"}
 
-if [ "$ACTION" = "build" ]
-  then
-    docker build -t rl-env .
+if [ "$ACTION" = "build" ]; then
+  docker build -t rl-env .
 fi
 
-if [ "$ACTION" = "restart" ]
-  then
-    docker kill "$(docker ps | grep "rl-env" | awk '{ print $1 }')"
-    docker run --rm -d -e PYTHONPATH=/root/mount -v ${PWD}:/root/mount -t -p 8889:8888 bidder-rl-env
+if [ "$ACTION" = "start" ]; then
+  docker run --rm -d -e PYTHONPATH=/root/mount -v ${PWD}:/root/mount -p 8889:8888 -p 6006:6006 -t rl-env
+fi
+
+if [ "$ACTION" = "restart" ]; then
+  docker kill "$(docker ps | grep "rl-env" | awk '{ print $1 }')"
+  docker run --rm -d -e PYTHONPATH=/root/mount -v ${PWD}:/root/mount -p 8889:8888 -p 6006:6006 -t rl-env
+fi
+
+if [ "$ACTION" = "shell" ]; then
+  docker exec -it "$(docker ps | grep "rl-env" | awk '{ print $1 }')" /bin/bash
 fi
